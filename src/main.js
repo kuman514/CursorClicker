e1d7ac6ba74d3c77146d8a9a09a0a8cca953d02c
kumankoishi.py <7iggif@naver.com>
2026-01-10 01:47:45 +0900

    draft: make code prettier

--- src/main.js
+++ src/main.js
@@ -6,22 +6,22 @@ const badges = [
     weight: 90,
     sizeRange: [0.8, 1.25],
     image: new URL('../assets/icons/cursor.png', import.meta.url).href,
-    sound: new URL('../assets/sounds/cursor.m4a', import.meta.url).href
+    sound: new URL('../assets/sounds/cursor.m4a', import.meta.url).href,
   },
   {
     name: 'koishi',
     weight: 5,
     sizeRange: [0.9, 1.15],
     image: new URL('../assets/icons/koishi.png', import.meta.url).href,
-    sound: new URL('../assets/sounds/koishi.m4a', import.meta.url).href
+    sound: new URL('../assets/sounds/koishi.m4a', import.meta.url).href,
   },
   {
     name: 'hoshino',
     weight: 5,
     sizeRange: [0.9, 1.15],
     image: new URL('../assets/icons/hoshino.png', import.meta.url).href,
-    sound: new URL('../assets/sounds/hoshino.m4a', import.meta.url).href
-  }
+    sound: new URL('../assets/sounds/hoshino.m4a', import.meta.url).href,
+  },
 ];
 
 function pickBadge() {
